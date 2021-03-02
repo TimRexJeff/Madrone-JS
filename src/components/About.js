8ca64dc59b62e29e8da0bd1a37ac8eed651c3577
tim-jeffries <timothyrexjeffries@gmail.com>
2021-03-01 19:20:24 -0800

    Complete About.js HTML layout

---
+++ src/components/About.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+function About() {
+    return(
+        <main>
+            <h2>Executive Team</h2>
+
+            <section>
+                <img/>
+                <article>
+                    <div>
+                        <h3>JAKE CAMPBELL</h3>
+                        <h4>Co-Founder, CEO</h4>
+                    </div>
+                    <p>Jake comes to Madrone with over 20 years of experience running IT service companies and consulting on national projects in both the healthcare and public sectors. Jake has always found a deep interest in technology, starting with programing at just 6 years of age. Today, he leads the vision and direction of Madrone Technology Group.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <div>
+                        <h3>ALEX HART</h3>
+                        <h4>Co-Founder, CTO</h4>
+                    </div>
+                    <p>As Chief Technology Officer at Madrone, Alex is responsible for driving technology strategies and projects. Alex has enjoyed building teams, companies and highly-available technology infrastructure for the last two decades. Before that, he engaged with building computers and servicing the technology needs of schools, businesses and individuals.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <div>
+                        <h3>TOM HARRISON</h3>
+                        <h4>Co-Founder, COO</h4>
+                    </div>
+                    <p>Tom grew up in Silicon Valley in an environment that fostered learning by doing.  By 7 years old, Tom had built his first computer and network. He has been found incubating teams on technical projects, both in and out of the workplace, for over 20 years. Madrone values Tom’s experience and expertise to keep the day-to-day operations running.</p>
+                </article>
+            </section>
+
+            <h2>Featured Partners</h2>
+                <article>
+                    <a></a>
+                    <a></a>
+                </article>
+        </main>
+    )
+}
+
+export default About
