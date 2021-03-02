932f55d46ce98e926cb81e7d023063451e982aba
tim-jeffries <timothyrexjeffries@gmail.com>
2021-03-01 19:08:13 -0800

    Complete Services.js HTML layout

---
+++ src/components/Services.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+function Services() {
+    return(
+        <main>
+            <section>
+                <img/>
+                <article>
+                    <h3>ENDPOINT MANAGEMENT</h3>
+                    <p>Proactive monitoring and management. Our support agent provides us with real-time monitoring, patch management and security software, so that we can address issues before they are problems that impact your workforce.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <h3>MANAGED NETWORK INFRASTRUCTURE AND DESIGN</h3>
+                    <p>Response before the call. We monitor your network devices for availability and response time - all while responding in real-time.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <h3>VOICE SERVICES</h3>
+                    <p>Managed IP voice services, including cloud or on-premise PBX, with affordable handsets, unlimited domestic calling and competitive international rates. Custom tailored solutions, to meet any needs.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <h3>CLOUD ARCHITECTURE & DEVOPS DESIGN AND SUPPORT SERVICES</h3>
+                    <p>Staffing to development and everything in-between - we’re your one stop shop for DevOps, architecture and support. Providing access to real experts, and real developers, in one place.</p>
+                </article>
+            </section>
+
+            <section>
+                <img/>
+                <article>
+                    <h3>ADVANCED NETWORKING</h3>
+                    <p>Design, implementation and support for complex and large networks. Providing expertise spanning locales or connecting infrastructure to cloud environments.</p>
+                </article>
+            </section>
+        </main>
+    )
+}
+
+export default Services
